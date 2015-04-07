e8a02a37dec87d663d020be5f201ce7275bebe53
fullace <fullacetripleflush@gmail.com>
2015-04-08 01:28:10 +0200

    add a splashscreen animation

--- Catcher-Code/src/com/full/ace/CGame.java
+++ Catcher-Code/src/com/full/ace/CGame.java
@@ -1,16 +1,29 @@
 package com.full.ace;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CGame extends Game {
-
 	private static final String TITLE = "Catcher";
-	private static final float VERSION = 0.1f;
-	private static final int VERSION_NUMBER = 1;
+	private static final float VERSION = 0.2f;
+	private static final int VERSION_NUMBER = 2;
+	public SpriteBatch batch;
+	public OrthographicCamera camera;
 	
 	@Override
-	public void create() {
-		//1
+	public void create () {
+		batch = new SpriteBatch();
+		camera=new OrthographicCamera();
+		this.setScreen(new SplashScreen(this));
 	}
 
+	@Override
+	public void render () {
+		super.render();
+		
+	}
 }
