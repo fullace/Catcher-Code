1c987c616f4ac55acb60e2b37aa7043da1c832d6
fullace <fullacetripleflush@gmail.com>
2015-04-07 22:56:29 +0200

    version 0.1

--- Catcher-code/src/com/full/ace/CGame.java
+++ Catcher-code/src/com/full/ace/CGame.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.Game;
 
 public class CGame extends Game {
 
+	private final static String TITLE = "Catcher";
+	private final static float VERSION = 0.1f;
+	private final static int VERSION_NUMBER = 1;
+			
 	@Override
 	public void create() {
-		
+		//more to come
 	}
 
 }
